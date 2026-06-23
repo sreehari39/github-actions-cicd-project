1c2b592fb2ad329c5c89cdcec30bd310ae021e75
Dezerv <dezerv@Dezervs-MacBook-Pro.local>
2026-06-23 22:47:01 +0530

    Testing Render auto deployment

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This is from GitHub Actions CI/CD pipeline!');
+  res.send('Render Auto Deploy Test Successful');
 });
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
